318bfb323573af28ae6c73cebe4c8238405aead4
kostyantin Varenye <kostyve@ac.sce.ac.il>
2020-04-10 10:47:56 +0300

    added eslint

--- src/experiment.test.js
+++ src/experiment.test.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-undef */
 const sum = require('./experiment');
 
-//test('adds 1 + 2 to equal 3', () => {
+// test('adds 1 + 2 to equal 3', () => {
 //    expect(sum(1, 2)).toBe(3);
-//});
-
-test('adds 1 + 2 to equal 3', sumTest);
+// });
 
 function sumTest() {
-    expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 }
+test('adds 1 + 2 to equal 3', sumTest);

00e6b634bff84ca76e1c71c88b3f34e29d67d7c4
kostyantin Varenye <kostyve@ac.sce.ac.il>
2020-05-24 19:45:38 +0300

    added the storage upload picture working right

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -1,61 +1,58 @@
 /* eslint-disable no-undef, no-global-assign, no-unused-vars, no-undef */
 
 var uploader = document.getElementById('uploader');
+//selecting an img ref
+var imgItem = document.querySelector('.upload-pic');
+auth.onAuthStateChanged( user => {
+  if (user){
+    db.collection('users').doc(user.uid).get().then((doc) =>{
+      //console.log(doc.data());
+      imgItem.src = doc.data().imgUrl
+    })
+  }
+})
+  
+
+
 
 //listen for file selection
 
 fileButton.addEventListener('change', function(e) {
 
-var file = e.target.files[0];
-var userId ='';
-//sketchy try catch doesnt work
-try{userId = auth.currentUser.uid}catch(err){console.log(err.code)}
-var imgPath = 'img/' + userId;
+  var file = e.target.files[0];
+  var userId ='';
+  //sketchy try catch doesnt work
+  try{userId = auth.currentUser.uid}catch(err){console.log(err.code)}
+  var imgPath = 'img/' + userId;
 
-var storageRef = cloudStorage.ref(imgPath + '/' + file.name);
+  var storageRef = cloudStorage.ref(imgPath + '/' + file.name);
 
-var task = storageRef.put(file);
+  var task = storageRef.put(file);
 
-task.on('state_changed',
+  task.on('state_changed',
 
-function progress(snapshot){
-    var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    uploader.style.width = percentage +'%';
-},
-function error(err){
-  console.log(err)
-},
+  function progress(snapshot){
+      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      uploader.style.width = percentage +'%';
+  },
+  function error(err){
+    console.log(err)
+  },
 
-function complete(){
-  
-  //Create a reference under which you want to list
-  //create full path for the url
-  storageRef.getMetadata().then(metadata => {
-    db.collection('users').doc(userId).update({
-      imgUrl: metadata.fullPath
-    }).then(() => {
+  function complete(){
+    task.snapshot.ref.getDownloadURL().then(downloadURL => {
+      console.log('File available at:' , downloadURL)
+      db.collection('users').doc(userId).update({
+        isVerified: true,
+        imgUrl: downloadURL
+      }).then(()=>{
+          console.log('verified student', userId)
+      }).catch(err => {
+        console.log(err.messege)
+      });
+    });
     
-    }).catch(err => {
-      console.log(err.messege);
-    })
-  })
-  
-  db.collection('users').doc(userId).update({
-    isVerified: true
-  }).then(()=>{
-      console.log('verified student')
-  }).catch(err => {
-    console.log(err.messege)
-  })
-  alert("Uploaded successfully!");
-  //create the data we need
-  
-  //update user db to include the url to the uploaded photo
+    alert("Uploaded successfully!");
 
-  //update user profile to verified
-
-  //give user permissions to order an apartment*/
-}
-
-);
+  });
 })
